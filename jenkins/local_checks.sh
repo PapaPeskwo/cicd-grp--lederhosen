48ae8ae55310fa52c6a27ef488690f592ee4ee28
daniel <daniel.brunomatzui@yh.nackademin.se>
2023-11-15 14:42:30 +0100

    Added checks in script

--- jenkins/local_checks.sh
+++ jenkins/local_checks.sh
@@ -2,9 +2,39 @@
 
 echo "Running local checks..."
 
-# Example: Run pylint
+# Run pylint
+echo "Running pylint..."
 docker run --rm pingurl:latest pylint /backend
 
-# Example: Build Docker image locally
+# Run pylint and fail if the score is under 9.0
+echo "Running pylint with threshold..."
+docker run --rm pingurl:latest pylint --fail-under=9.0 /backend
+
+# Check the exit status of the pylint command
+pylintStatus=$?
+
+if [ $pylintStatus -ne 0 ]; then
+    echo "Error: Pylint score is below the threshold of 9.0"
+    exit 1
+fi
+
+# Build Docker image locally
+echo "Building Docker image..."
 docker build --no-cache -t pingurl:latest .
 
+# Run the application and check if it starts successfully
+echo "Running the application..."
+docker run --rm -d -p 5000:5000 --name pingapp pingurl:latest
+
+# Sleep for a moment to allow the application to start
+sleep 5
+
+# Check if the application container is running
+if [ "$(docker inspect -f '{{.State.Running}}' pingapp)" != "true" ]; then
+    echo "Error: The application failed to start."
+    exit 1
+fi
+
+# Check code formatting using black
+echo "Checking code formatting with black..."
+docker run --rm pingurl:latest black --check /backend
